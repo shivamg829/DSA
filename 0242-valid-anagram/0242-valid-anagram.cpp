de6c7e62e8e1f7c8a292a37ac944849e5742dab7
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-06-17 14:08:13 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(t.begin(), t.end());
-        sort(s.begin(), s.end());
-        return s==t;
+        if(s.size()!=t.size()) return false;
+        int ch[26] = {0};
+        for(int i = 0; i<s.size(); i++){
+            ch[s[i]-'a']++;
+        }for(int i = 0; i<t.size(); i++){
+            ch[t[i]-'a']--;
+            if(ch[t[i]-'a']<0) return false;
+        }
+        return true;
     }
 };
