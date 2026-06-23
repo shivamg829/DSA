e1c1aafbb7ae521071ff1aabd5806f0a7dfc7d06
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-06-23 19:31:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (64.4%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,23 +3,16 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int l = 0;
         int r = numbers.size()-1;
-        if(numbers.size()==2){
-            if(numbers[0]+numbers[1]==target){
-                return {1, 2};
-            }
-        }
-        while(l<r){
-            int sum = numbers[l] + numbers[r];
+        while(l<=r){
+            int sum = numbers[l]+ numbers[r];
             if(sum==target){
-                return {l+1,r+1};
-            }
-            else if(sum>target){
+                return {l+1 , r+1};
+            }else if(sum>target){
                 r--;
-            }
-            else{
+            }else{
                 l++;
             }
         }
-        return {};
+        return {-1, -1};
     }
 };
