e4add830d5c0c30ac988bb25b31a2d2bb113945f
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-02 10:36:59 +0530

    Time: 9 ms (76.37%), Space: 35 MB (88.52%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int numofdays(vector<int>& weights, int cap){
+    int noOfDay(vector<int>&weights, int cap){
         int day = 1;
         int load = 0;
         int n = weights.size();
         for(int i = 0; i<n; i++){
             if(load+weights[i]>cap){
+                load =weights[i];
                 day++;
-                load=weights[i];
-            }
-            else{
+            }else{
                 load+=weights[i];
             }
-        }return day;
+        } 
+        return day;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        while(low<=high){
-            int mid = low+(high -low)/2;
-            int nodays = numofdays(weights, mid);
-            if(nodays<=days){
-                high = mid-1;
+        int l = *max_element(weights.begin(),weights.end());
+        int h = accumulate(weights.begin(), weights.end(), 0);
+        while(l<=h){
+            int m = l+(h-l)/2;
+            int noOfD = noOfDay(weights, m);
+            if(noOfD>days){
+                l=m+1;
+            }else{
+                h=m-1;
             }
-            else{
-                low = mid+1;
-            }
-        }return low;
+        }
+        return l;
     }
 };
