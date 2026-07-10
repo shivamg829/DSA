9a2d24b7acb63a788a6f979b1d1adc9ea9fbfafe
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-10 21:04:24 +0530

    Time: 62 ms (5.02%), Space: 17.8 MB (98.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isCycle(int src, vector<bool>&vis, vector<bool>&recPath, vector<vector<int>>& prerequisites){
+        vis[src]=true;
+        recPath[src]=true;
+        for(int i = 0; i<prerequisites.size(); i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            if(u == src){ // FIX 1: Only traverse edges starting from src
+                if(!vis[v]){
+                    if(isCycle(v, vis, recPath, prerequisites)){
+                        return true;
+                    }
+                }else if(recPath[v]){
+                    return true;
+                }
+            }
+        }
+        recPath[src] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool>vis(numCourses, false);
+        vector<bool>recPath(numCourses, false);
+        for(int i = 0; i<numCourses; i++){ 
+            if(!vis[i]){
+                if(isCycle(i, vis, recPath, prerequisites)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
