f2327ec39c1d34b8ec7e91139e9d9b6764566002
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-13 10:11:00 +0530

    Time: 15 ms (28.51%), Space: 17.7 MB (20.25%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>dic(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool>dp(n+1, false);
+        dp[0] = true;
+        for(int i = 0; i<=n; i++){
+            for(int j = 0; j<i; j++){
+                string word = s.substr(j , i-j);
+                if(dp[j] && dic.count(word)){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
