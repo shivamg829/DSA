bb236df0dbe07617a22df7e261a4337c0687f7c1
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-21 11:32:47 +0530

    Time: 0 ms (100%), Space: 10.1 MB (26.89%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,8 +14,8 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         while(fast!=NULL && fast->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
         return slow;
     }
