4a7c88ef040b26c7edbd02b5aed06eb8b1f3f94d
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-23 16:10:23 +0530

    Time: 17 ms (30.77%), Space: 38.7 MB (19.01%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int atMost(vector<int>& nums, int goal) {
-        if (goal < 0) {
-            return 0;
-        }
-
-        int left = 0;
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        mp[0] = 1;
         int sum = 0;
         int count = 0;
-
-        for (int right = 0; right < nums.size(); right++) {
-            sum += nums[right];
-
-            while (sum > goal) {
-                sum -= nums[left];
-                left++;
+        
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            if(mp.find(sum - goal) != mp.end()){
+                count += mp[sum - goal];
             }
-
-            count += right - left + 1;
+            mp[sum]++;
         }
-
+        
         return count;
-    }
-
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal - 1);
     }
 };
