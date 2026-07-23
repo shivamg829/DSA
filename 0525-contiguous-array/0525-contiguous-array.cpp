4e8137bde938ee04de15f1d7c2b1acfc86697a54
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-23 15:59:35 +0530

    Time: 24 ms (56.37%), Space: 88.3 MB (44.01%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -5,19 +5,19 @@ public:
         unordered_map<int, int>mp;
         mp[0]=-1;
         int preSum = 0;
-        int len = 0;
-        for(int i= 0; i<n; i++){
+        int maxLen = 0;
+        for(int i = 0; i<n; i++){
             if(nums[i]==0){
                 preSum+=-1;
             }else{
                 preSum+=1;
             }
             if(mp.find(preSum)!=mp.end()){
-                len = max(len, i - mp[preSum]);
+                maxLen = max(maxLen, i-mp[preSum]);
             }else{
-                mp[preSum] = i;
+                mp[preSum]=i;
             }
         }
-        return len;
+        return maxLen;
     }
 };
