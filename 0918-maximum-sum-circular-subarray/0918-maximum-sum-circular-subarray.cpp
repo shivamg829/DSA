99de3d27eab1182c6e716fcdf3b71d046f0ad133
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-26 15:24:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (49.33%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
         int maxSum = INT_MIN;
-        int minSum = INT_MAX;
-        int total = 0;
         int currMax = 0;
-        int currMin = 0;
         int n = nums.size();
+        int minSum = INT_MAX;
+        int currMin = 0;
+        int total = 0;
         for(int i = 0; i<n; i++){
-            total+=nums[i];
-            currMax = max(currMax+nums[i], nums[i]);
+            currMax = max(nums[i], nums[i]+currMax);
             maxSum = max(maxSum, currMax);
-            currMin = min(currMin+nums[i], nums[i]);
+            currMin = min(nums[i], currMin+nums[i]);
             minSum = min(minSum, currMin);
+            total+=nums[i];
         }
-        if (maxSum < 0){
-            return maxSum;
-        }
-        return max(maxSum, total-minSum);
+        if(maxSum < 0) return maxSum;
+        return max(maxSum , total-minSum);
+        
     }
 };
