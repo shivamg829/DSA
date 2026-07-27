a7edabb647c425d128e512ad96a8bbea7a45f466
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-27 10:51:20 +0530

    Time: 0 ms (100%), Space: 21.6 MB (95.54%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,15 +4,15 @@ public:
         int i = 0;
         int n = intervals.size();
         vector<vector<int>>res;
-        // Non overlap
+        // no overlap
         while(i<n && intervals[i][1]<newInterval[0]){
             res.push_back(intervals[i]);
             i++;
         }
         // overlap
         while(i<n && intervals[i][0]<=newInterval[1]){
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+            newInterval[1] = max(intervals[i][1], newInterval[1]);
             i++;
         }
         res.push_back(newInterval);
