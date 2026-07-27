23b88ee4b172536170286e90eb90d4b1528fb448
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-27 11:09:07 +0530

    Time: 121 ms (7.26%), Space: 106.6 MB (8.76%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -7,12 +7,14 @@ public:
         vector<int>temp = points[0];
         for(int i = 1; i<n; i++){
             vector<int>curr = points[i];
+            // if overlap
             if(curr[0]<=temp[1]){
-                temp[1] = min(temp[1], curr[1]);
+                temp[1] = min(curr[1], temp[1]);
             }else{
                 cnt++;
                 temp = curr;
             }
+
         }
         return cnt;
     }
