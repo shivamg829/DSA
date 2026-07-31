effbf0e11368124f0711cbadd48fd4102dc7cadb
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-07-31 21:34:55 +0530

    Time: 25 ms (39.83%), Space: 107.5 MB (18.46%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,9 +5,10 @@ public:
         vector<int>ans(n, 0);
         stack<int>st;
         for(int i = 0; i<n; i++){
-            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
-                ans[st.top()]=i-st.top();
+            while(!st.empty() && temperatures[i]>temperatures[st.top()]){
+                int idx = st.top();
                 st.pop();
+                ans[idx]=i-idx;
             }
             st.push(i);
         }
