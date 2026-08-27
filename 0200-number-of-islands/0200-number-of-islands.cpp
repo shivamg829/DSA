374dfe84df2f7200ed8dffb67542800ec2fa6810
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-08-27 19:30:20 +0530

    Time: 25 ms (71.74%), Space: 24.3 MB (19.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>&vis){
         int n = grid.size();
         int m = grid[0].size();
         vis[row][col]=1;
-        queue<pair<int, int>>q;
-        q.push({row , col});
+        queue<pair<int,int>>q;
+        q.push({row, col});
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
@@ -13,29 +13,28 @@ public:
             int dr[] = {-1, 0, 1, 0};
             int dc[] = {0, 1, 0, -1};
             for(int i = 0; i<4; i++){
-                int nrow = r+dr[i];
-                int ncol = c+dc[i];
-                if(nrow<n && ncol<m && nrow>=0 && ncol>=0 && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                    q.push({nrow, ncol});
+                int nrow = r + dr[i];
+                int ncol = c + dc[i];
+                if(nrow<n && ncol<m && nrow>=0 && ncol>=0 && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
                     vis[nrow][ncol]=1;
+                    q.push({nrow, ncol});
                 }
             }
         }
     }
-    
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int cnt = 0;
+        int res = 0;
         vector<vector<int>>vis(n, vector<int>(m, 0));
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    cnt++;
-                    bfs(i, j, vis, grid);
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    res++;
+                    bfs(i, j, grid, vis);
                 }
             }
         }
-        return cnt;
+        return res;
     }
 };
