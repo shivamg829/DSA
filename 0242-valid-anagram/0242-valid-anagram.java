96ee76d4acbe15b9cc6d6b0b6e736572b9d805f4
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-09-07 22:13:16 +0530

    Time: 4 ms (83.67%), Space: 44.2 MB (89.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int [] freq = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i = 0; i<s.length(); i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i = 0; i<s.length(); i++){
+            freq[t.charAt(i)-'a']--;
+            if(freq[t.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
