9c785b3fe955a22b91bf870bf851891437736254
Shivam Gangwar <shivamgangwar2197@gmail.com>
2026-09-17 08:56:21 +0530

    Time: 0 ms (100%), Space: 47.7 MB (98.04%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int h = s.length-1;
+        while(l<=h){
+            char ch = s[l];
+            s[l] = s[h];
+            s[h] = ch;
+            h--;
+            l++;
+        }
+    }
+}
